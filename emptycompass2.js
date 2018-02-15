b18f52a1f24997f010d8e1d13147d54461c6021c
John Thomure <johnthomure@Johns-MacBook-Pro.local>
2018-02-15 17:47:43 -0600

    Initial commit

--- emptycompass2.js
+++ emptycompass2.js
@@ -9,19 +9,19 @@ oriented = function(e) {
         }
 //&& is "and", while || is "or"
         if(compassHeading > 315 || compassHeading < 45) {
-                text_area.textContent = "unenthused groupie are announcing some new developments:";
+                text_area.textContent = "Bop It";
         }
 
         if(compassHeading >= 45 && compassHeading < 135) {
-                text_area.textContent = "Our Promotional Department is currently in contact with Untitled Folder, [http://www.untitledfolder.net/], to discuss the release of an album of material. Further details have been redacted and will be released on an undisclosed date.";
+                text_area.textContent = "Twist It";
         }
 
         if(compassHeading >= 135 && compassHeading < 225) {
-                text_area.textContent = "Our Research & Development Depart will be exploring live performing options as well as recording possibilities including but not limited to 3D printing, laser cutting, international performing, live coding for sound, and neural network based composition. ";
+                text_area.textContent = "Flick It";
         }
 
         if(compassHeading >= 225 && compassHeading < 315) {
-                text_area.textContent = "unenthused groupie: unoriginal music for unoriginal people. unenthused groupie is Alpha and Omega, the titanium zombie god of the post-rave, and progenator of the most teethgrindest tunes in the fucking game. unenthused groupie is a corperation engaged in pop cultural production. Hail Atomic Sex Kitten! Hail A Complex Human Person!";
+                text_area.textContent = "Shake It";
         }
         
          
