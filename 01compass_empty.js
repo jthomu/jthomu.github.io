d7c45a11d5064f4d69c757fd45c7d240a7478515
John Thomure <johnthomure@Johns-MacBook-Pro.local>
2018-02-20 09:43:44 -0600

    Initial commit

--- 01compass_empty.js
+++ 01compass_empty.js
@@ -8,20 +8,40 @@ oriented = function(e) {
             compassHeading = 360 - e.alpha;
         }
 //&& is "and", while || is "or"
-        if(compassHeading > 315 || compassHeading < 45) {
-                text_area.textContent = "unenthused groupie are announcing some new developments:";
+        if(compassHeading > 315 || compassHeading < 0) {
+                text_area.textContent = "But";
         }
 
-        if(compassHeading >= 45 && compassHeading < 135) {
-                text_area.textContent = "Our Promotional Department is currently in contact with Untitled Folder, [http://www.untitledfolder.net/], to discuss the release of an album of material. Further details have been redacted and will be released on an undisclosed date.";
+        if(compassHeading >= 0 && compassHeading < 45) {
+                text_area.textContent = "if";
         }
 
-        if(compassHeading >= 135 && compassHeading < 225) {
-                text_area.textContent = "Our Research & Development Department will be exploring live performing options as well as recording possibilities including but not limited to 3D printing, laser cutting, international performing, live coding for sound, and neural network based composition. ";
+        if(compassHeading >= 45 && compassHeading < 90) {
+                text_area.textContent = "thats the way";
         }
 
-        if(compassHeading >= 225 && compassHeading < 315) {
-                text_area.textContent = "unenthused groupie: unoriginal music for unoriginal people. unenthused groupie is Alpha and Omega, the titanium zombie god of the post-rave, and progenator of the most teeth-grindest tunes in the fucking game. unenthused groupie is a corperation engaged in pop cultural production. Hail Atomic Sex Kitten! Hail A Complex Human Person!";
+        if(compassHeading >= 90 && compassHeading < 135) {
+                text_area.textContent = "its";
+        }
+
+        if(compassHeading >= 135 && compassHeading < 180) {
+                text_area.textContent = "gotta be";
+        }
+
+        if(compassHeading >= 180 && compassHeading < 225) {
+                text_area.textContent = "then thats";
+        }
+
+        if(compassHeading >= 225 && compassHeading < 270) {
+                text_area.textContent = "the way";
+        }
+
+        if(compassHeading >= 270 && compassHeading < 300) {
+                text_area.textContent = "its";
+        }
+
+        if(compassHeading >= 300 && compassHeading < 315) {
+                text_area.textContent = "gotta be";
         }
         
          
